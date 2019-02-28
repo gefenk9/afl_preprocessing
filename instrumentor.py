4995e06ccf1b979cbf3419c454f148bb67cdaf47
gefen keinan <gefenk9@gmail.com>
2019-02-28 15:55:24 +0200

    working with stdin

--- instrumentor.py
+++ instrumentor.py
@@ -1,10 +1,14 @@
+
 with open('example_1.c') as f:
     lines = f.readlines()
 
 new_lines = lines.copy()
-for index, line in enumerate(lines):
+for line in lines:
+    if lines.index(line) == 0:
+        # globals
+        new_lines.insert(new_lines.index(line), '#include <stdio.h>\n')
     if "pthread_mutex_lock" in line:
-        new_lines.insert(index, 'sleep(1);\n')
+        new_lines.insert(new_lines.index(line), 'float sec=0.0;\n scanf("%f", &sec);\n if (sec >= 0 && sec <= 10) sleep(sec/10);\nprintf("%f", sec)')
 
 with open('exmaple_1_instrumented.c', 'w') as fw:
     for line in new_lines:
