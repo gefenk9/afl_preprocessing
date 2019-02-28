218b29627a523cec55a5a706ac6d02d9775b1891
gefen keinan <gefenk9@gmail.com>
2019-02-28 17:43:38 +0200

    adde dlist of indicators except "pthread_mutex_lock" -> will be updated with more experiemnts running

--- instrumentor.py
+++ instrumentor.py
@@ -1,4 +1,5 @@
 
+sleep_indicators = ["pthread_mutex_lock"]
 with open('./examples/example_1.c') as f:
     lines = f.readlines()
 
@@ -7,8 +8,9 @@ for line in lines:
     if lines.index(line) == 0:
         # globals
         new_lines.insert(new_lines.index(line), '#include <stdio.h>\n')
-    if "pthread_mutex_lock" in line:
-        new_lines.insert(new_lines.index(line), 'float sec=0.0;\n scanf("%f", &sec);\n if (sec >= 0 && sec <= 10) sleep(sec/10);\n')
+    for indicator in sleep_indicators:
+        if indicator in line:
+            new_lines.insert(new_lines.index(line), 'float sec=0.0;\n scanf("%f", &sec);\n if (sec >= 0 && sec <= 10) sleep(sec/10);\n')
 
 with open('./examples/example_1_instrumented.c', 'w') as fw:
     for line in new_lines:
