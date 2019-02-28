b26c5a764d54b0a279df6e858fc0c1d4f2724ddf
gefen keinan <gefenk9@gmail.com>
2019-02-28 18:08:32 +0200

    updated the run.sh script to compile the afl and the santiizer after we instrument the sleep commands according to the user input, the AFL input

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -4,4 +4,5 @@ mkdir input
 echo "" >input/test
 mkdir output
 afl-gcc examples\after\example_1_instrumented.c -pthread -fsanitize=thread
+
 TSAN_OPTIONS="abort_on_error=1" afl-fuzz -m none  -i input/ -o output/  ~/Downloads/afl_preprocessing/a.out
