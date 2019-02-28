da9f7dee3e32a1bc8ba305af78e90d4cc5745c88
gefen keinan <gefenk9@gmail.com>
2019-02-28 17:54:42 +0200

    added more generic wat to run a lot of examples

--- instrumentor.py
+++ instrumentor.py
@@ -1,19 +1,26 @@
+import os
 
 sleep_indicators = ["pthread_mutex_lock"]
-with open('./examples/example_1.c') as f:
-    lines = f.readlines()
 
-new_lines = lines.copy()
-for line in lines:
-    if lines.index(line) == 0:
-        # globals
-        new_lines.insert(new_lines.index(line), '#include <stdio.h>\n')
-    for indicator in sleep_indicators:
-        if indicator in line:
-            new_lines.insert(new_lines.index(line), 'float sec=0.0;\n scanf("%f", &sec);\n if (sec >= 0 && sec <= 10) sleep(sec/10);\n')
+for root, dirs, files in os.walk("./examples/before"):
+    for name in files:
+        file_path = (os.path.join(root, name))
 
-with open('./examples/example_1_instrumented.c', 'w') as fw:
-    for line in new_lines:
-        fw.write(line)
+
+        with open(file_path) as f:
+            lines = f.readlines()
+
+        new_lines = lines.copy()
+        for line in lines:
+            if lines.index(line) == 0:
+                # globals
+                new_lines.insert(new_lines.index(line), '#include <stdio.h>\n')
+            for indicator in sleep_indicators:
+                if indicator in line:
+                    new_lines.insert(new_lines.index(line), 'float sec=0.0;\n scanf("%f", &sec);\n if (sec >= 0 && sec <= 10) sleep(sec/10);\n')
+
+        with open('./examples/after/'+name+'instrumented.c', 'w') as fw:
+            for line in new_lines:
+                fw.write(line)
 
 
