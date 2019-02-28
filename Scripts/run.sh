32bba357968a972da9b21e5d49c6ca67263f6d40
gefen keinan <gefenk9@gmail.com>
2019-02-28 17:16:43 +0200

    new shell script running automatic the instrumented python script to the examples c code files

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -2,4 +2,5 @@ cd ~/Downloads/afl_preprocessing
 python3.7 ./instrumentor.py
 gcc examples/example_1_instrumented.c -pthread -fsanitize=thread
 mkdir input
+mkdir output
 TSAN_OPTIONS="abort_on_error=1" afl-fuzz -m none  -i input/ -o output/  ~/Downloads/a.out
