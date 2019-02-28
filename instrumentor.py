748baf5980efaddb7bba8afa3f6b998306a2373f
gefen keinan <gefenk9@gmail.com>
2019-02-28 18:15:04 +0200

    updated the run.sh script to compile the afl and the santiizer after we instrument the sleep commands according to the user input, the AFL input

--- instrumentor.py
+++ instrumentor.py
@@ -19,7 +19,7 @@ for root, dirs, files in os.walk("./examples/before"):
                 if indicator in line:
                     new_lines.insert(new_lines.index(line), 'float sec=0.0;\n scanf("%f", &sec);\n if (sec >= 0 && sec <= 10) sleep(sec/10);\n')
 
-        with open('./examples/after/'+name+'instrumented.c', 'w') as fw:
+        with open('./examples/after/'+name.split('.')[0]+'_instrumented.c', 'w') as fw:
             for line in new_lines:
                 fw.write(line)
 
