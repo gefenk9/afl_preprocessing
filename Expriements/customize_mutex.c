dd3a0eda700f7bed7d6093a6e6b9e8c93c8a978e
gefen keinan <gefenk9@gmail.com>
2019-03-04 11:18:11 +0200

    edited mutex

--- Expriements/customize_mutex.c
+++ Expriements/customize_mutex.c
@@ -25,16 +25,11 @@ void *Thread1(void* x) {
 	Global=1;
 enterLock(&G_lock);
 leaveLock(&G_lock);
-	//sleep(sleep_sec);
-//	pthread_mutex_lock(&lock);
-//	pthread_mutex_unlock(&lock);
   	return NULL;
 }
 
 void *Thread2(void* x) {
-  //sleep(sleep_sec);
-//  pthread_mutex_lock(&lock);
-  //pthread_mutex_unlock(&lock);
+
 enterLock(&G_lock);
 leaveLock(&G_lock);
   Global=2;  
@@ -58,13 +53,11 @@ int main(int argc, char *argv[])
 		printf("%s", out);
 		free(out);
 	} else if (strcmp(input, "\n") == 0) {
-		  pthread_mutex_init(&lock, NULL); 
 		  pthread_t t[2];
 		  pthread_create(&t[0], NULL, Thread1, NULL);
 		  pthread_create(&t[1], NULL, Thread2, NULL);
 		  pthread_join(t[0], NULL);
 		  pthread_join(t[1], NULL);
-		 pthread_mutex_destroy(&lock);
 	} else {
 		fprintf(stderr, "Usage: %s\nText utility - accepts commands on stdin and prints results to stdout:\n", argv[0]);
 		fprintf(stderr, "\tInput           | Output\n");
