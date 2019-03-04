5633a38538a239640af4a5794cbe10ce2f7c9a4c
gefen keinan <gefenk9@gmail.com>
2019-03-04 11:53:43 +0200

    edited mutex

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -3,5 +3,5 @@ mkdir input
 echo "" >input/test
 rm -r output
 mkdir output
-afl-gcc examples/after/example_1_instrumented.c -pthread -fsanitize=thread
+afl-gcc $PWD/examples/after/example_1_instrumented.c -pthread -fsanitize=thread
 TSAN_OPTIONS="abort_on_error=1" afl-fuzz -m none -t 1500 -i input/ -o output/  $PWD/a.out
